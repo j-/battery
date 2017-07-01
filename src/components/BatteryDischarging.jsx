8577b76674e90ba6987dd021d6ff2e3e5a687404
j- <j@skeoh.com>
2017-07-01 16:34:30 +1000

    Use a color gradient for the discharging battery component

--- src/components/BatteryDischarging.jsx
+++ src/components/BatteryDischarging.jsx
@@ -5,11 +5,27 @@ import {
 	batteryDischarging,
 } from './styles';
 
+const getColorForLevel = (level) => {
+	let value, hue;
+	if (level < 0.25) {
+		value = level / 0.25;
+		hue = value * 50;
+	} else {
+		value = (level - 0.25) / 0.75
+		hue = 50 + value * 60;
+	}
+	return `hsl(${hue}, 60%, 80%)`;
+};
+
 const BatteryDischarging = ({
 	level,
 	...props
 }) => (
-	<div className={ batteryDischarging } { ...props }>
+	<div
+		className={ batteryDischarging }
+		style={{ backgroundColor: getColorForLevel(level) }}
+		{ ...props }
+	>
 		<BatteryLevel level={ level } />
 	</div>
 );
--- stories/BatteryDischarging.jsx
+++ stories/BatteryDischarging.jsx
@@ -5,21 +5,14 @@ import BatteryDischarging from '../src/components/BatteryDischarging';
 
 storiesOf('BatteryDischarging', module)
 	.addDecorator(background)
-	.add('100%', () => (
-		<BatteryDischarging level={1} />
-	))
-	.add('80%', () => (
-		<BatteryDischarging level={0.8} />
-	))
-	.add('60%', () => (
-		<BatteryDischarging level={0.6} />
-	))
-	.add('40%', () => (
-		<BatteryDischarging level={0.4} />
-	))
-	.add('20%', () => (
-		<BatteryDischarging level={0.2} />
-	))
-	.add('0%', () => (
-		<BatteryDischarging level={0} />
-	));
+	.add('100%', () => <BatteryDischarging level={1.0} />)
+	.add('90%',  () => <BatteryDischarging level={0.9} />)
+	.add('80%',  () => <BatteryDischarging level={0.8} />)
+	.add('70%',  () => <BatteryDischarging level={0.7} />)
+	.add('60%',  () => <BatteryDischarging level={0.6} />)
+	.add('50%',  () => <BatteryDischarging level={0.5} />)
+	.add('40%',  () => <BatteryDischarging level={0.4} />)
+	.add('30%',  () => <BatteryDischarging level={0.3} />)
+	.add('20%',  () => <BatteryDischarging level={0.2} />)
+	.add('10%',  () => <BatteryDischarging level={0.1} />)
+	.add('0%',   () => <BatteryDischarging level={0.0} />);
