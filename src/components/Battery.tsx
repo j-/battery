84b00467e339fbb0567ae29c32b93e1ca3a6d196
j- <j@skeoh.com>
2018-11-23 15:26:43 +1100

    Convert battery component to TypeScript

---
+++ src/components/Battery.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import BatteryUnknown from './BatteryUnknown';
+import BatteryCharging from './BatteryCharging';
+import BatteryDischarging from './BatteryDischarging';
+import BatteryFull from './BatteryFull';
+
+export interface Props {
+	known: boolean;
+	charging: boolean | null;
+	full: boolean;
+	level: number | null;
+}
+
+const Battery: React.StatelessComponent<Props> = ({
+	known,
+	charging,
+	full,
+	level,
+}) => (
+	// Battery level is unknown
+	!known   ? <BatteryUnknown /> :
+	// Battery is full
+	full     ? <BatteryFull /> :
+	// Battery is charging
+	charging ? <BatteryCharging level={level!} /> :
+	// Battery is discharging
+	<BatteryDischarging level={level!} />
+);
+
+export default Battery;
